438e5fc52186bc51079ad72a431c1df74945ba3d
xxAROX <arox.email.0@gmail.com>
2023-04-24 22:12:27 +0200

    Changes: - Implemented ProxySettingsForm, that works lol. - Restructured to a module plugin thing - Added test plugin module
    Signed-off-by: xxAROX <arox.email.0@gmail.com>

--- WD-Forms-Plugin/src/main/java/xxAROX/WDForms/WDForms.java
+++ WD-Forms-Plugin/src/main/java/xxAROX/WDForms/WDForms.java
@@ -5,6 +5,7 @@ import dev.waterdog.waterdogpe.network.protocol.ProtocolCodecs;
 import dev.waterdog.waterdogpe.player.ProxiedPlayer;
 import dev.waterdog.waterdogpe.plugin.Plugin;
 import lombok.Getter;
+import org.apache.commons.lang3.RandomUtils;
 import xxAROX.WDForms.forms.types.ProxySettingsForm;
 
 import java.util.ArrayList;
@@ -16,11 +17,14 @@ public class WDForms extends Plugin {
     private static WDForms instance;
     public static WDForms getInstance() {return instance;}
     final private static HashMap<ProxiedPlayer, FormPlayerSession> sessions = new HashMap<>();
+    @Getter private static long settings_ticker_id;
     @Getter final private static List<Function<ProxiedPlayer, ProxySettingsForm.ProxySettingsFormBuilder>> globalSettings = new ArrayList<>();
 
 
     @Override public void onStartup() {
         instance = this;
+        settings_ticker_id = RandomUtils.nextLong() /1000 *1000;
+        saveResource("config.yml");
     }
     @Override public void onEnable() {
         getProxy().getEventManager().subscribe(PlayerLoginEvent.class, (event -> {
