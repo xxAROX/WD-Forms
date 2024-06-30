f843386859ef2c590ef255c68280fee67af7b1d9
xxAROX <arox.email.0@gmail.com>
2024-06-30 13:47:07 +0200

    Updated to new waterdog snapshot
    Signed-off-by: xxAROX <arox.email.0@gmail.com>

--- WD-Forms-Plugin/src/main/java/xxAROX/WDForms/FormPacketHandler.java
+++ WD-Forms-Plugin/src/main/java/xxAROX/WDForms/FormPacketHandler.java
@@ -1,16 +1,13 @@
 package xxAROX.WDForms;
 
-import dev.waterdog.waterdogpe.network.PacketDirection;
 import dev.waterdog.waterdogpe.network.protocol.handler.PluginPacketHandler;
-import org.cloudburstmc.protocol.bedrock.data.AttributeData;
+import org.cloudburstmc.protocol.bedrock.PacketDirection;
 import org.cloudburstmc.protocol.bedrock.packet.*;
 import org.cloudburstmc.protocol.common.PacketSignal;
 
-import java.util.List;
-
 public record FormPacketHandler(FormPlayerSession session) implements PluginPacketHandler {
     @Override public PacketSignal handlePacket(BedrockPacket packet, PacketDirection direction) {
-        if (direction.equals(PacketDirection.FROM_USER)) {
+        if (direction.equals(PacketDirection.SERVER_BOUND)) { // Before: FROM_USER
             if (packet instanceof NetworkStackLatencyPacket networkStackLatencyPacket) {
                 if (networkStackLatencyPacket.getTimestamp() == WDForms.getSettings_ticker_id()) {
                     return session.handleProxySettings_response(session.getPlayer());
@@ -19,13 +16,13 @@ public record FormPacketHandler(FormPlayerSession session) implements PluginPack
             else if (packet instanceof ModalFormResponsePacket modalFormResponsePacket) return modalFormResponsePacket.getFormId() < 0 ? session.response(modalFormResponsePacket) : PacketSignal.UNHANDLED;
             else if (packet instanceof ServerSettingsResponsePacket serverSettingsResponsePacket) return serverSettingsResponsePacket.getFormId() < 0 ? session.response(serverSettingsResponsePacket) : PacketSignal.UNHANDLED;
             else if (packet instanceof ServerSettingsRequestPacket) session.handleProxySettings_request(session.getPlayer());
-        } else {
+        }/* else {
             if (packet instanceof UpdateAttributesPacket updateAttributesPacket) {
                 session.setEntityRuntimeId(updateAttributesPacket.getRuntimeEntityId());
                 List<AttributeData> attributes = updateAttributesPacket.getAttributes().stream().filter(attributeData -> attributeData.getName().equals("minecraft:player.level")).toList();
                 if (attributes.size() == 1) session.setLevel(attributes.get(0).getValue());
-            }
-        }
+            } DOESN'T WORK
+        }*/
         return PacketSignal.UNHANDLED;
     }
 }
--- WD-Forms-Plugin/src/main/java/xxAROX/WDForms/WDFormsProtocolUpdater.java
+++ WD-Forms-Plugin/src/main/java/xxAROX/WDForms/WDFormsProtocolUpdater.java
@@ -3,6 +3,7 @@ package xxAROX.WDForms;
 import dev.waterdog.waterdogpe.network.protocol.updaters.ProtocolCodecUpdater;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockPacketDefinition;
+import org.cloudburstmc.protocol.bedrock.data.PacketRecipient;
 import org.cloudburstmc.protocol.bedrock.packet.ModalFormRequestPacket;
 import org.cloudburstmc.protocol.bedrock.packet.ModalFormResponsePacket;
 import org.cloudburstmc.protocol.bedrock.packet.ServerSettingsRequestPacket;
@@ -14,10 +15,10 @@ public class WDFormsProtocolUpdater implements ProtocolCodecUpdater {
         BedrockPacketDefinition<ModalFormRequestPacket> modalFormRequestDefinition = baseCodec.getPacketDefinition(ModalFormRequestPacket.class);
         BedrockPacketDefinition<ServerSettingsRequestPacket> serverSettingsRequestDefinition = baseCodec.getPacketDefinition(ServerSettingsRequestPacket.class);
         BedrockPacketDefinition<ServerSettingsResponsePacket> serverSettingsResponseDefinition = baseCodec.getPacketDefinition(ServerSettingsResponsePacket.class);
-        builder.registerPacket(ModalFormResponsePacket::new, modalFormResponseDefinition.getSerializer(), modalFormResponseDefinition.getId());
-        builder.registerPacket(ModalFormRequestPacket::new, modalFormRequestDefinition.getSerializer(), modalFormRequestDefinition.getId());
-        builder.registerPacket(ServerSettingsRequestPacket::new, serverSettingsRequestDefinition.getSerializer(), serverSettingsRequestDefinition.getId());
-        builder.registerPacket(ServerSettingsResponsePacket::new, serverSettingsResponseDefinition.getSerializer(), serverSettingsResponseDefinition.getId());
+        builder.registerPacket(ModalFormResponsePacket::new, modalFormResponseDefinition.getSerializer(), modalFormResponseDefinition.getId(), PacketRecipient.BOTH);               // SERVER
+        builder.registerPacket(ModalFormRequestPacket::new, modalFormRequestDefinition.getSerializer(), modalFormRequestDefinition.getId(), PacketRecipient.BOTH);                  // CLIENT
+        builder.registerPacket(ServerSettingsRequestPacket::new, serverSettingsRequestDefinition.getSerializer(), serverSettingsRequestDefinition.getId(), PacketRecipient.BOTH);   // CLIENT
+        builder.registerPacket(ServerSettingsResponsePacket::new, serverSettingsResponseDefinition.getSerializer(), serverSettingsResponseDefinition.getId(), PacketRecipient.BOTH);// SERVER
         return builder;
     }
 }
